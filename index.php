83eec821cd55244e9ab9f1e6b40dde002320c03b
Jonasjunior00 <jonas_junior2009@hotmail.com>
2020-10-14 14:54:52 -0300

    comite cartografia 2

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<p>cartografia</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <p>cartografia</p>
+    <hr>
+    <h1>xxxxxxxxxxxxxxx</h1>
+
+</body>
+
+</html>
